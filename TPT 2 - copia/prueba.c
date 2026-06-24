0b8513a33456230d72a064818dea5013bcc2eafa
josuear201 <tomasjosueflores@gmail.com>
2026-06-24 15:34:13 -0300

    prueba.c

--- TPT 2 - copia/prueba.c
+++ TPT 2 - copia/prueba.c
@@ -10,6 +10,19 @@ int main(){
 	automata* A = ing_automata(f);
 	mostrar_automata(A);
 	fclose(f);
+	// Prueba cadenas
+	char cadena_prueba[100];
+	printf("\nIngrese una cadena para validar: ");
+	scanf("%s", cadena_prueba);
+	
+	// Valida dependiendo al automata
+	if (A->det == 1) {
+		printf("\nDetectado: Automata Determinista (DFA)\n");
+		aceptar_cadena_dfa(A, cadena_prueba);
+	} else {
+		printf("\nDetectado: Automata No Determinista (NFA)\n");
+		aceptar_cadena_nfa(A, cadena_prueba);
+	}
 	
 	return 0;
 }
